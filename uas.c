b6815f477a39e13fc88b75483c663345d274f352
inelutfiatulhanifah <133070067+inelutfiatulhanifah@users.noreply.github.com>
2023-05-10 09:54:21 +0700

    Update uas.c
    menu

--- uas.c
+++ uas.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+void menu()
+{
+    printf("1. Tambahkan buku\n");
+    printf("2. Cari buku\n");
+    printf("3. Pinjam buku\n");
+    printf("4. Kembalikan buku\n");
+    printf("5. Keluar\n");
+}
+
 int main()
 {
   
