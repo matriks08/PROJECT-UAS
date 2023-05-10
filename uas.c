fa2dfc4b7c1ef59e5351fa35be4ec77a84b00a9f
inelutfiatulhanifah <133070067+inelutfiatulhanifah@users.noreply.github.com>
2023-05-10 10:02:37 +0700

    Update uas.c

--- uas.c
+++ uas.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+typedef struct data_buku
+{
+    char judul[60];
+    char penerbit[60];
+    char pengarang[60];
+    int halaman;
+    int tahun;
+    int tersedia;
+} buku_buku;
+
+void menu();
+
+int main()
+{
+  
+}
+
 void menu()
 {
     printf("1. Tambahkan buku\n");
@@ -9,7 +26,3 @@ void menu()
     printf("5. Keluar\n");
 }
 
-int main()
-{
-  
-}
